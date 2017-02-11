4020a8f9e1798c84525246c9e1a4f3df7b8902b5
megadude <ivinogra@epic24562.local>
2017-02-11 15:09:07 -0600

    Initial Commit

---
+++ FrozenQuotes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FrozenQuotes
+//
+//  Created by Ilya Vinogradov on 2/11/17.
+//  Copyright © 2017 Ilya Vinogradov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FrozenQuotes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FrozenQuotes
+//
+//  Created by Ilya Vinogradov on 2/11/17.
+//  Copyright © 2017 Ilya Vinogradov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
